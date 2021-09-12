03abd2a2da5623643ef280b850125f2152d6d155
hiaffan <90562676+hiaffan@users.noreply.github.com>
2021-09-12 18:11:09 +0400

    Add files via upload

---
+++ new format.py
@@ -0,0 +1,61 @@
+print("hi this so a riddle game\n solve all the qeuestions you can\n if you dont know the answer then type idk\n good luck\n")
+
+r = input("what is 1 + 1?\n ans: ")
+if r == "idk":
+    print("sry wrong ans\n totla points: 0")
+elif r == "2" or "11":
+    r = input("what is 68 + 1? \n ans: ")
+    if r == "idk":
+        print("sry wronf ans\n total ponits: 1")
+    elif r == "69":
+        r = input("Which word becomes shorter when you add 2 letter to it?\n ans: ")
+        if r == "idk":
+            print("sry wrong ans\n totla points: 2")
+        elif r == "short":
+            r = input("Im tall when im young and ,shorter when im older? What am I?\n ans:")
+            if r == "idk":
+                print("sry wrong ans\n total poinys: 3\n")
+            elif r == "a pencil" or "a candel":
+                r = input("what is the capital of france?\n ans: ")
+                if r == "idk":
+                    print("sry worng ans\n total points: 4")
+                elif r == "F" or "f":
+                    r = input("When Grant was 8, his brother was half his age. Now, Grant is 14. How old is his brother?\n ans: ")
+                    if r == "idk":
+                        print("sry wrong ans\n total points: 5")
+                    elif r == "10":
+                        r = input("I repeat only the last word you say. The more I repeat, the softer I got. I cannot be seen but can be heard what am I?\n ans: ")
+                        if r == "idk":
+                            print('sry wrong ans\n total points: 6')
+                        elif r == "an echo" or "echo":
+                            r = input("I can fly but have no wings. I can cry but I have no eyes. Wherever I go, darkness follows me. What am I?\n ans: ")
+                            if r == "idk":
+                                print("sry wrong ans\n total points: 7")
+                            elif r == "cloud" or "clouds":
+                                r= input("The more you take, the more you leave behind? what am I?\n ans: ")
+                                if r == "idk":
+                                    print("sry wrong ans\n total points: 8")
+                                elif r == "foot steps" or "footsteps":
+                                    r = input("I have cities but no houses, forests but no trees, water but no fish. What am I?\n ans: ")
+                                    if r == "idk":
+                                        print("sry wront ans\n total points: 9")
+                                    elif r == "a map" or "map":
+                                         print("Well done you win!!\n total points: 10 ")
+                                         print("game made by:\n Affan ali")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
